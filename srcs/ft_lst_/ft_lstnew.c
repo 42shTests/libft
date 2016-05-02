5ac7691f7908e9b770e8ce10ffaa88f07c8446de
Adrien Nouvel <adrien.nouvel@outlook.com>
2016-05-02 13:40:26 +0200

    replace malloc with ft_memalloc

--- srcs/ft_lst_/ft_lstnew.c
+++ srcs/ft_lst_/ft_lstnew.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anouvel <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: anouvel <adrien.nouvel@outlook.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/11/10 14:21:10 by anouvel           #+#    #+#             */
-/*   Updated: 2016/02/25 16:23:07 by anouvel          ###   ########.fr       */
+/*   Created: 2016/05/02 13:37:57 by anouvel           #+#    #+#             */
+/*   Updated: 2016/05/02 13:40:06 by anouvel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,11 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list *list;
 
-	list = (t_list *)malloc(sizeof(t_list));
-	if (!list)
+	if (!(list = (t_list *)malloc(sizeof(t_list))))
 		return (NULL);
 	if (content)
 	{
-		list->content = malloc(sizeof(content) * content_size);
-		ft_bzero(list->content, content_size);
+		list->content = ft_memalloc(sizeof(content) * content_size);
 		list->content = ft_memcpy(list->content, content, content_size);
 		list->content_size = content_size;
 	}
