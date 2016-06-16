42dc7bd453ef45165d29aaa94bc170f000bc70de
Jean-Michel Gigault <jgigault@student.42.fr>
2016-06-16 21:55:34 +0200

    optimize ft_strjoin3

--- srcs/ft_str_/ft_strjoin3.c
+++ srcs/ft_str_/ft_strjoin3.c
@@ -3,15 +3,24 @@
 char			*ft_strjoin3(char const *s1, char const *s2, char const *s3)
 {
 	char		*new;
+	size_t		len;
+	size_t		i;
 
-	if (s1 && s2)
+	if (s1 && s2 && s3)
 	{
-		new = ft_strnew(ft_strlen(s1) + ft_strlen(s2) + ft_strlen(s3) + 1);
+		new = ft_strnew(ft_strlen(s1) + ft_strlen(s2) + ft_strlen(s3));
 		if (new)
 		{
-			new = ft_strcat(new, s1);
-			new = ft_strcat(new, s2);
-			new = ft_strcat(new, s3);
+			len = -1;
+			i = -1;
+			while(s1[++i])
+				new[++len] = s1[i];
+			i = -1;
+			while(s2[++i])
+				new[++len] = s2[i];
+			i = -1;
+			while(s3[++i])
+				new[++len] = s3[i];
 			return (new);
 		}
 	}
