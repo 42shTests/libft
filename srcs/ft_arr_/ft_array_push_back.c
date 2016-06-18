decf1e63b3a389af84330474f2cf8dc74db38ab2
Jean-Michel Gigault <jgigault@student.42.fr>
2016-06-18 12:59:27 +0200

    fix description

--- srcs/ft_arr_/ft_array_push_back.c
+++ srcs/ft_arr_/ft_array_push_back.c
@@ -3,8 +3,8 @@
 /*
 ** This function duplicates the value `*value` and push it at the end
 ** of the array pointed by `***array` through an allocation of a new one.
-** If the array is NULL, a new one is created.
 ** If either parameter is NULL, it returns -1.
+** If the array pointed by `***array` is NULL, a new one is created.
 ** On failure, the function returns -1 and `***array` is not changed.
 ** On success, the function returns the new size of the array.
 */
