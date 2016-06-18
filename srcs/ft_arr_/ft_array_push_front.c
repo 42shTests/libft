a7b9ad2a190714dd4a95ba5bc84c375952337529
Jean-Michel Gigault <jgigault@student.42.fr>
2016-06-18 12:52:16 +0200

    new function ft_array_push_front

--- srcs/ft_arr_/ft_array_push_front.c
+++ srcs/ft_arr_/ft_array_push_front.c
@@ -1,7 +1,7 @@
 #include "libft.h"
 
 /*
-** This function duplicates the value `*value` and push it at the end
+** This function duplicates the value `*value` and push it at the beginning
 ** of the array pointed by `***array` through an allocation of a new one.
 ** If the array is NULL, a new one is created.
 ** On failure, the function returns -1 and `***array` is not changed.
@@ -19,7 +19,7 @@ static int	s_return_and_free(char **new_array, size_t total)
 	return (-1);
 }
 
-int			ft_array_push_back(char ***array, char const *value)
+int			ft_array_push_front(char ***array, char const *value)
 {
 	char	**new_array;
 	size_t	total;
@@ -32,17 +32,17 @@ int			ft_array_push_back(char ***array, char const *value)
 			total++;
 	if ((new_array = (char **)malloc(sizeof(char *) * (total + 2))) == NULL)
 		return (s_return_and_free(new_array, 0));
+	if ((new_array[0] = ft_strdup(value)) == NULL)
+		return (s_return_and_free(new_array, 0));
 	total = 0;
 	if (*array)
 		while ((*array)[total])
 		{
-			if ((new_array[total] = ft_strdup((*array)[total])) == NULL)
+			if ((new_array[total + 1] = ft_strdup((*array)[total])) == NULL)
 				return (s_return_and_free(new_array, total));
 			total++;
 		}
-	if ((new_array[total] = ft_strdup(value)) == NULL)
-		return (s_return_and_free(new_array, total));
-	new_array[total + 1] = NULL;
+	new_array[++total] = NULL;
 	ft_memdel_tab((void ***)&(*array));
 	*array = new_array;
 	return (total);
